9d620f128fc34e323dbcf2130d3740dfd0ab3161
DevJunior1997 <165954516+andreanjunior@users.noreply.github.com>
2024-05-14 19:34:47 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+import Navbar from "./components/Navbar/Navbar";
+import TaskList from "./components/TaskList/TaskList";
+
+let idAcc = 0;
+const generateId = () => {
+  idAcc = idAcc + 1;
+  return idAcc;
+};
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (title, state) => {
+    console.log("funçao sendo chamada em App");
+    const newTask = {
+      id: generateId(),
+      title,
+      state,
+    };
+    setTasks((existingTasks) => {
+      return [...existingTasks, newTask];
+    });
+  };
+
+  const updateTask = (id, title, state) => {
+    console.log("update task sendo chamada");
+    setTasks((existingTasks) => {
+      return existingTasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, title, state };
+        } else {
+          return task;
+        }
+      });
+    });
+  };
+
+  const deleteTask = (id) => {
+    setTasks((existingTasks) => {
+      return existingTasks.filter((task) => task.id !== id);
+    });
+  };
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="container">
+        <TaskList
+          title="Pendente"
+          onAddTask={addTask}
+          taskState="Pendente"
+          tasks={tasks.filter((t) => t.state === "Pendente")}
+          onTaskUpdate={updateTask}
+          onDeleteTask={deleteTask}
+        />
+        <TaskList
+          title="Fazendo"
+          onAddTask={addTask}
+          taskState="Fazendo"
+          tasks={tasks.filter((t) => t.state === "Fazendo")}
+          onTaskUpdate={updateTask}
+          onDeleteTask={deleteTask}
+        />
+        <TaskList
+          title="Completa"
+          onAddTask={addTask}
+          taskState="Completa"
+          tasks={tasks.filter((t) => t.state === "Completa")}
+          onTaskUpdate={updateTask}
+          onDeleteTask={deleteTask}
+        />
+      </div>
+    </div>
+  );
+}
